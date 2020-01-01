12e875017b38bdf77dd726f8aecff1b663bb9fcd
Flavio <flaviofelicianocabral@gmail.com>
2020-01-01 14:31:52 -0300

    Criado atribudos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,20 @@
 class Pessoa:
+    def __init__(self, nome = None, idade=58):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Feliciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Flavio'
+    print(p.nome)
+    print(p.idade)
+
+
 
